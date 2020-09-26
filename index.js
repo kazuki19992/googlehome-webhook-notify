28ce3b1ceb04bad9656d85fcaf86d8137a2d7dc9
kazuki19992 <kushida98@gmail.com>
2020-09-27 00:24:20 +0900

    うーんこうんこ、うごかねぇ〜！wwwwwwwwww

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
+// const http = require('http');
+
+// const googlehome = require('google-home-notifier');
+// const language = 'ja';
+
+// // GoogleHomeを指定
+// // ファイルリーダー
+// // let reader = new FileReader();
+
+// // // text形式でgooglehomeのデバイス名を読み込む
+// // reader.readAsText('googlehome.txt');
+// // let deviceName = reader.result;
+// let deviceName = 'リビングルーム';
+
+// // // text形式でしゃべらせる言葉を読み込む
+// // reader.readAsText('talktext.txt');
+// let talkText = 'うんこ';
+
+// // // GoogleHome端末のIPアドレスを取得
+// // reader.readAsText('deviceIp.txt');
+// // let ipAddress = reader.result;
+// let ipAddress = '192.168.1.9';
+
+// // // デプロイ環境のアドレスを取得
+// // reader.readAsText('address.txt');
+// // let deployAddress = reader.result;
+// let deployAddress = '192.168.1.2';
+
+
+// googlehome.device(deviceName, language);
+// googlehome.ip(ipAddress);
+
+// http.createServer(function (req, res){
+//     req.on("end", function() {
+//         // しゃべらせる
+//         googlehome.notify(talkText, function(res) {
+//             console.log(res);
+//         });
+//     });
+
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end();
+// }).listen(3000, deployAddress);
+
+
 const http = require('http');
 
 const googlehome = require('google-home-notifier');
-const language = 'ja';
+googlehome.device('リビングルーム', 'ja');
+googlehome.ip('192.168.1.9');
 
-// GoogleHomeを指定
-// ファイルリーダー
-let reader = new FileReader();
-
-// text形式でgooglehomeのデバイス名を読み込む
-reader.readAsText('googlehome.txt');
-let deviceName = reader.result;
-
-// text形式でしゃべらせる言葉を読み込む
-reader.readAsText('talktext.txt');
-let talkText = reader.result;
-
-// GoogleHome端末のIPアドレスを取得
-reader.readAsText('deviceIp.txt');
-let ipAddress = reader.result;
-
-// デプロイ環境のアドレスを取得
-reader.readAsText('address.txt');
-let deployAddress = reader.result;
-
-
-googlehome.device(deviceName, language);
-googlehome.ip(ipAddress);
-
-http.createServer(function (req, res){
+http.createServer(function (req, res) {
     req.on("end", function() {
-        // しゃべらせる
-        googlehome.notify(talkText, function(res) {
+        googlehome.notify('うんこ', function(res) {
             console.log(res);
         });
     });
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end();
-}).listen(3000, deployAddress);
+
+}).listen(3000, '192.168.1.2');
